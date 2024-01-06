67763b769c5c17bfd79bb1e7c8b7ba97cd16a878
pablogarc <juan.garo29si@gmail.com>
2024-01-05 19:31:10 -0600

    Add onboarding page component

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import { generalStyles } from "../../../lib/styles";
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${generalStyles.headText}`}>Onboarding</h1>
+    </main>
+  );
+}
