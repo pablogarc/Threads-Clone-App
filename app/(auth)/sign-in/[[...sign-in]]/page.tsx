120607600c2c5a9fba017986d815a096442a6b9d
pablogarc <juan.garo29si@gmail.com>
2024-01-05 19:31:17 -0600

    Add sign-in and sign-up pages

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In"
+};
+
+export default function Page() {
+  return <SignIn />;
+}
