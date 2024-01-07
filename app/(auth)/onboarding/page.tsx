dc77d657332ffee781ea5b8a533ea4048e187ef3
pablogarc <juan.garo29si@gmail.com>
2024-01-06 21:09:00 -0600

    Refactor onboarding page component

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,9 +1,30 @@
-import { generalStyles } from "../../../lib/styles";
+import AccountProfile from "@/components/forms/AccountProfile";
+import { User } from "@/lib/definitions";
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Page() {
+  const user = await currentUser();
+  const userInfo: any = {};
+
+  const userData: User = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName || "",
+    bio: userInfo?.bio || "",
+    image: userInfo?.image || user?.imageUrl,
+  }
+
   return (
-    <main>
-      <h1 className={`${generalStyles.headText}`}>Onboarding</h1>
+    <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+      <h1 className="text-heading2-bold text-light-1">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Threads
+      </p>
+
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
     </main>
   );
 }
