1ab2a98440f68607df341613cd40cde3da08f193
pablogarc <juan.garo29si@gmail.com>
2024-01-07 20:06:36 -0600

    Add ThreadCardProps, ParamsUpdateUser, and ParamsCreateThread types

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -6,3 +6,43 @@ export type User = {
   bio?: string;
   image?: string;
 };
+
+export type ThreadCardProps = {
+  id: string;
+  currentUserId: string;
+  content: string;
+  author: {
+    name: string;
+    image: string;
+    id: string;
+  };
+  community: {
+    id: string;
+    name: string;
+    image: string;
+  } | null;
+  createdAt: string;
+  parentId: string | null;
+  comments: {
+    author: {
+      image: string;
+    };
+  };
+  isComment?: boolean;
+};
+
+export type ParamsUpdateUser = {
+  userId?: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+};
+
+export type ParamsCreateThread = {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+};
