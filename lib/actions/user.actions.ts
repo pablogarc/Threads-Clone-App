c0f52e234fa9315e14512317d94f968fc5b31df6
pablogarc <juan.garo29si@gmail.com>
2024-01-07 20:07:41 -0600

    Add updateUser and fetchUser functions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,3 +1,44 @@
 "use server";
 
-export async function user() {}
+import { connectDB } from "../moongose";
+import User from "../models/user.model";
+import { revalidatePath } from "next/cache";
+import { ParamsUpdateUser } from "../definitions";
+
+export async function updateUser({
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
+}: ParamsUpdateUser): Promise<void> {
+  connectDB();
+
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      { username: username.toString(), name, bio, image, onboarded: true },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+}
+
+export async function fetchUser(userId: string) {
+  connectDB();
+
+  try {
+    return await User.findOne({ id: userId });/* .populate({
+      path: "communities",
+      model: Community,
+    }) */
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`)
+  }
+}
