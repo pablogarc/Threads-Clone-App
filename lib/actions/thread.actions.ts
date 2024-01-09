a93b7d8b0f68c23544846fcf122dc4ddd8fff8d0
pablogarc <juan.garo29si@gmail.com>
2024-01-08 19:19:27 -0600

    Add fetchThreadById and addCommentToThread functions

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ParamsCreateThread } from "../definitions";
+import { ParamsAddCommentToThread, ParamsCreateThread } from "../definitions";
 import { connectDB } from "../moongose";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
@@ -64,5 +64,80 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
     return { posts, isNext };
   } catch (error: any) {
     throw new Error(`Error fetch posts: ${error.message}`);
+  }
+}
+
+export async function fetchThreadById(id: string) {
+  connectDB();
+
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Error fetch Thread by Id: ${error.message}`);
+  }
+}
+
+export async function addCommentToThread({
+  threadId,
+  commentText,
+  userId,
+  path,
+}: ParamsAddCommentToThread) {
+  connectDB();
+
+  try {
+    // Find the original thread by its ID
+    const originalThread = await Thread.findById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    // Create a new thread with the comment text
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    // Save the new thread
+    const saveCommentThread = await commentThread.save();
+
+    // update the original thread to include the new comment
+    originalThread.children.push(saveCommentThread._id);
+
+    // save the original thread
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error Add Comment to Thread: ${error.message}`);
   }
 }
